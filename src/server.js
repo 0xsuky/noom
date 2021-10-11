d0c015396f819a59ef3186f8ffe85011277d7958
suky <0xsuky@gmail.com>
2021-10-11 22:47:42 +0900

    Feat: #2.11 구현

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIo from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -12,7 +13,13 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIo(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+instrument(wsServer, { auth: false });
 
 function publicRooms() {
   const {
